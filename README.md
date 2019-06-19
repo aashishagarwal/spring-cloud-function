2e04d050f2449f72ddc1d5c517fb4dbf69963d5b
Ashish Agarwal <aashish.agarwal@gmail.com>
2019-06-18 21:06:23 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# spring-cloud-function"
+
+- Run CloudfunctionApplication class
+- curl localhost:8080/reverseString/hello
---
+++ src/main/java/com/test/cloud/function/CloudFunctionApplication.java
@@ -0,0 +1,21 @@
+package com.test.cloud.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class CloudFunctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFunctionApplication.class, args);
+    }
+
+    @Bean
+    public Function<String, String> reverseString() {
+        return value -> new StringBuilder(value).reverse().toString();
+    }
+
+}
